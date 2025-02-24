b11869fd834c8ea41ca3711f970e3334118cdbec
Oscar M. Olivera <oscarmolivera@gmail.com>
2025-02-23 20:05:41 -0400

    [Entities] Update Scaffold Recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_23_235215) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_24_000454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "recipe_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
